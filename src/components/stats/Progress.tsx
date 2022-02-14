ee939f90c8ec4cb47428a88108477665859b1c47
NicosHere <31162757+NicosHere@users.noreply.github.com>
2022-02-13 19:43:27 -0800

    Update Progress.tsx

--- src/components/stats/Progress.tsx
+++ src/components/stats/Progress.tsx
@@ -11,7 +11,7 @@ export const Progress = ({ index, size, label }: Props) => {
       <div className="rounded w-full ml-2">
         <div
           style={{ width: `${5 + size}%` }}
-          className="bg-green-600 text-xs font-medium text-green-100 text-center p-0.5 rounded"
+          className="bg-green-500 text-xs font-medium text-green-100 text-center p-0.5 rounded"
         >
           {label}
         </div>
