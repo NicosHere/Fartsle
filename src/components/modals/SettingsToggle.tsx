953f162d03aecb38e9ecea1ea78d28756599316a
NicosHere <31162757+NicosHere@users.noreply.github.com>
2022-02-13 17:33:13 -0800

    More Theming

--- src/components/modals/SettingsToggle.tsx
+++ src/components/modals/SettingsToggle.tsx
@@ -8,13 +8,13 @@ type Props = {
 
 export const SettingsToggle = ({ settingName, flag, handleFlag }: Props) => {
   const toggleHolder = classnames(
-    'w-14 h-8 flex items-center bg-neutral-300 rounded-full p-1 duration-300 ease-in-out cursor-pointer',
+    'w-14 h-8 flex items-center bg-neutral-300 rounded-md p-1 duration-300 ease-in-out cursor-pointer',
     {
       'bg-green-400': flag,
     }
   )
   const toggleButton = classnames(
-    'bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out cursor-pointer',
+    'bg-white w-6 h-6 rounded-md shadow-md transform duration-300 ease-in-out cursor-pointer',
     {
       'translate-x-6': flag,
     }
