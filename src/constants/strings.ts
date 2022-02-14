af06a683c9ebed18411324f0866b131c7fba0910
NicosHere <31162757+NicosHere@users.noreply.github.com>
2022-02-13 16:23:47 -0800

    Built
    YO!

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,25 +1,25 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const ABOUT_GAME_MESSAGE = 'About this game'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['You Farted!', 'Farts!', 'Good Farts!']
+export const GAME_COPIED_MESSAGE = 'Farts copied to clipboard'
+export const ABOUT_GAME_MESSAGE = 'About this Farts'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough Farts'
+export const WORD_NOT_FOUND_MESSAGE = 'Farts not found'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'Fart Mode can only be enabled at the start!'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `The Farts was ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
   `Must use ${guess} in position ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
-export const ENTER_TEXT = 'Enter'
+  `Farts must contain ${letter}`
+export const ENTER_TEXT = 'Farts'
 export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
+export const STATISTICS_TITLE = 'Fartsistics'
+export const GUESS_DISTRIBUTION_TEXT = 'Guess Fartstribution'
+export const NEW_WORD_TEXT = 'New Farts in'
 export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
+export const TOTAL_TRIES_TEXT = 'Total Farts'
+export const SUCCESS_RATE_TEXT = 'Fartsess rate'
 export const CURRENT_STREAK_TEXT = 'Current streak'
 export const BEST_STREAK_TEXT = 'Best streak'
